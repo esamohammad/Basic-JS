71783a5943633ba087c57f4c4098e7c8086bfc18
mdesabd <esa.ce2020@gmail.com>
2022-11-05 22:57:41 +0600

    1st

--- basic.js
+++
@@ -1,1 +0,0 @@
-<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
---
+++ variable.js
@@ -0,0 +1,40 @@
+// Variable
+
+var x = 100;
+console.log(x);
+
+
+// numeric variable or number variable
+var y = 500;
+var z = 1000;
+var m = 10000;
+
+
+var name = "akash"; //srting variable!
+
+var israining = true; //bulian variable! 
+
+var isgoing = false; //bulian variable! 
+
+//(Addition , Multiplication, Substraction, Divition )
+
+// Addition
+console.log ("Adition");
+console.log (x + y + z);
+console.log (x + m);
+console.log (m + z);
+
+
+// Multiplication
+console.log ("Multiplication"); 
+console.log (x * z);
+
+
+// Divition
+console.log ("Division");
+console.log (z / x);
+
+
+// Subtract
+console.log ("Subract");
+console.log(y - x);
