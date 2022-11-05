bcb98601b9f9fefec8a6ad0aa5663ea2930bf32a
mdesabd <esa.ce2020@gmail.com>
2022-11-06 01:54:07 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,26 @@
+// Comparison Oparator-- =,>,<, <=,>= etc.
+
+console.log(5 > 10);  //false
+
+console.log(5 < 10);  //true 
+
+console.log(10==10); //true 
+
+console.log(25 !=30); //true 
+
+console.log(30 !=30); //false
+        
+// abar var niyeo kaj kora jay... 
+
+var a=10; 
+var b=20; 
+var c=30; 
+var d=40; 
+
+console.log(a==b); 
+
+console.log(a> b);
+        
+console.log(a < b); 
+
+console.log(a !=b);
