792f23fccd9749dd4c4bd1d2f688d44d0c7db8f9
mdesabd <esa.ce2020@gmail.com>
2022-11-05 23:02:54 +0600

    2nd

---
+++ variableTypes.js
@@ -0,0 +1,13 @@
+// Variabale is---3 types.
+   
+
+    // 3 types is----
+
+    // a) Numaric
+
+    // b) String
+
+    // c) Boolian
+
+// Variable name must be one word, we can use camelcase letter - 
+// নামের প্রথমে কোন সংখা ব্যাবহার করা যাবে না। নাম অবশ্যই single word, if name is combind more than one word it will be camel case format. example---- ""dhakaMyCity""
