938721c80c16a021f4f95a41da61754133370e22
mdesabd <esa.ce2020@gmail.com>
2022-11-07 01:58:01 +0600

    set ok

---
+++ 24.feetConverter.js
@@ -0,0 +1,12 @@
+// A function inch to feet.
+
+
+function inchToFeet(inches) {
+    var feet = inches / 12;
+    return feet;
+}
+
+var inches = 156;
+var feet = inchToFeet(inches);
+console.log(feet);
+
